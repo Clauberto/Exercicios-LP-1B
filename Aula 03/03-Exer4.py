6bfe153d4365053f5bd078dfad965197e834e798
Clauberto <68787027+Clauberto@users.noreply.github.com>
2021-08-16 21:03:41 -0300

    Finalizado exercícios da aula 03

--- Aula 03/03-Exer4.py
+++ Aula 03/03-Exer4.py
@@ -2,4 +2,4 @@ n1 = (float(input('Qual seu primeiro número?')))
 n2 = (float(input('Qual seu segundo número?')))
 
 soma = n1+n2
-print(soma)
+print('A soma dos valores será {}'.format(soma))
--- Aula 03/03-Exer6.py
+++ Aula 03/03-Exer6.py
@@ -1,9 +1,8 @@
-""" a corrigir """
 n = int(input('Digite um número:'))
 dobro = n*2
 triplo = n*3
 raiz = n ** (1/2)
 
-print('O dobro de ' + n + ' vale ' + dobro)
-print('O triplo de ' + n + ' vale ' + triplo)
-print('A raiz de ' + n + ' vale ' + raiz)
+print('O dobro de {} é {}'.format(n,dobro))
+print('O triplo de {} é {}'.format(n,triplo))
+print('A raiz quadrada de {} é {}'.format(n,raiz))
