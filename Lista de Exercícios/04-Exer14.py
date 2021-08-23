dcb3c73ede398386cffa61e89bd65721b4025b8d
Clauberto <callsmeclauberto@gmail.com>
2021-08-23 17:09:02 -0300

    11212

--- Lista de Exercícios/04-Exer14.py
+++ Lista de Exercícios/04-Exer14.py
@@ -1,4 +1,13 @@
 #Faça um programa que leia um número inteiro e mostre na tela a sua tabuada.
-#Não feito com o for pois a professora solicitou.
+#Não feito com a estrutura for pois a professora solicitou.
 num = int(input("Digite o valor que deseja fazer a tabuada:"))
-print(num, "x = {}")
+print(num, "x 1 = {}".format(num))
+print(num, "x 2 = {}".format(num*2))
+print(num, "x 3 = {}".format(num*3))
+print(num, "x 4 = {}".format(num*4))
+print(num, "x 5 = {}".format(num*5))
+print(num, "x 6 = {}".format(num*6))
+print(num, "x 7 = {}".format(num*7))
+print(num, "x 8 = {}".format(num*8))
+print(num, "x 9 = {}".format(num*9))
+print(num, "x 10 = {}".format(num*10))
